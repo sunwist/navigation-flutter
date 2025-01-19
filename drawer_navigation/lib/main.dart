d126c5e538f0207bd2615edf3d026e69b6715d00
sunwist <oliveirajennifer2648@gmail.com>
2025-01-19 19:06:13 -0300

    :sparkles: feat: adiciona menu de navegação

--- drawer_navigation/lib/main.dart
+++ drawer_navigation/lib/main.dart
@@ -10,6 +10,101 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          ),
+        ),
+        backgroundColor: Color(0xFF6441a1),
+        iconTheme: const IconThemeData(
+        color: Colors.white,
+        ),
+      ),
+      drawer: const NavigatorDrawer(),
+    );
+  }
+}
+
+class NavigatorDrawer extends StatelessWidget {
+  const NavigatorDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Color(0XFF8760c5),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            buildHeader(context),
+            buildMenuItems(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildHeader(BuildContext context) {
+  return Container(
+    padding: EdgeInsets.only(
+      top: MediaQuery.of(context).padding.top,
+    ),
+  );
+}
+
+Widget buildMenuItems(BuildContext context) {
+  return Container(
+    padding: const EdgeInsets.all(24),
+    child: Wrap(
+      runSpacing: 16,
+      children: [
+        ListTile(
+          leading: const Icon(Icons.home_outlined, color: Colors.white),
+          title: const Text('Home', style: TextStyle(color: Colors.white)),
+          onTap: () => Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const HomePage(),
+            ),
+          ),
+        ),
+        ListTile(
+          leading: const Icon(Icons.favorite_border, color: Colors.white),
+          title: const Text('Favorites', style: TextStyle(color: Colors.white),),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const FavoritePage(),
+            ));
+          }
+        ),
+        ListTile(
+          leading: const Icon(Icons.workspaces_outlined, color: Colors.white),
+          title: const Text('Workflow', style: TextStyle(color: Colors.white),),
+          onTap: () {}
+        ),
+        ListTile(
+          leading: const Icon(Icons.update_outlined, color: Colors.white),
+          title: const Text('Updates', style: TextStyle(color: Colors.white),),
+          onTap: () {}
+        ),
+      ],
+    ),
+  );
 }
