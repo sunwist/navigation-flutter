4a82a6c371a883d18b9c52145bd75b7c6efabcdf
sunwist <oliveirajennifer2648@gmail.com>
2025-01-24 16:42:20 -0300

    :sparkles: feat: interface UserPage integra o widget ProfilePage e os dados para exibir o perfil do usuário

--- drawer_navigation/lib/pages/user_page.dart
+++ drawer_navigation/lib/pages/user_page.dart
@@ -1,11 +1,44 @@
+import 'package:drawer_navigation/utils/user_preferences.dart';
+import 'package:drawer_navigation/widgets/profile_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:drawer_navigation/main.dart';
 
-class UserPage extends StatelessWidget{
+class UserPage extends StatefulWidget{
   const UserPage({super.key});
 
   @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final user = UserPreferences.myUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: (){
+            Navigator.pushReplacement(context, 
+            MaterialPageRoute(builder: (context) => const HomePage()));
+          },
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Color(0XFF8760c5),
+        ),
+      ),
+      drawer: const NavigatorDrawer(),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          ProfileWidget(
+            imagePath: user.imagePath,
+            onClicked: () async {},
+          ),
+        ],
+      ),
+    );
   }
 }
