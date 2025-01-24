9f9dc297f7c704de392c7eb3952248bbe3c08623
sunwist <oliveirajennifer2648@gmail.com>
2025-01-24 16:34:14 -0300

    :sparkles: feat: adiciona dadosdo usuário padrão na classe UserPreferences

---
+++ drawer_navigation/lib/utils/user_preferences.dart
@@ -0,0 +1,9 @@
+import 'package:drawer_navigation/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath: 'https://images.unsplash.com/photo-1506863530036-1efeddceb993?q=80&w=1944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Sarah Abs',
+    email: 'sarah.abs@gmail.com',
+  );
+}
